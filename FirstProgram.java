474bab574c514d8ef6f2bb103057eb77085b5769
Ic3Venom <Ic3Venom@gmail.com>
2017-06-19 22:09:21 -0700

    Create FirstProgram.java

---
+++ FirstProgram.java
@@ -0,0 +1,13 @@
+public class FirstProgram {
+	public static void main( String [] args ) {
+
+		//Using the System.out.println(); method print the following information
+
+		System.out.println( "Programming is not a spectator sport!" );
+		System.out.println("Name: " + "Julian Meyn" );
+		System.out.println("Student Number: " + "1067829" );
+		System.out.println("Major: " + "Computer Science" );
+		System.out.println("Favorite Sport: " + "Swimming" );
+ 		System.exit( 0 );
+ 	}
+}
