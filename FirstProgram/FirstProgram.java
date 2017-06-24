8de302ab9997365570195aa7445996ff5659b127
Ic3Venom <Ic3Venom@gmail.com>
2017-06-23 22:16:52 -0700

    Update FirstProgram.java

--- FirstProgram/FirstProgram.java
+++ FirstProgram/FirstProgram.java
@@ -1,14 +1,17 @@
 package firstprogram;
 
 public class FirstProgram {
+    
     public static void main( String [] args ) {
-
 
         System.out.println( "Programming is not a spectator sport!" );
         System.out.println("Name: " + "Julian Meyn" );
         System.out.println("Student Number: " + "1067829" );
         System.out.println("Major: " + "Computer Science" );
         System.out.println("Favorite Sport: " + "Swimming" );
+    
         System.exit( 0 );
+
     }
+    
 }
