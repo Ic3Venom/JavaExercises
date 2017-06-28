b9335c83cc02264c602097cb80c414a653508c02
Ic3Venom <Ic3Venom@gmail.com>
2017-06-27 23:23:58 -0700

    Update RestaurantClient.java

--- restaurantclient/RestaurantClient.java
+++ restaurantclient/RestaurantClient.java
@@ -8,38 +8,37 @@ public class RestaurantClient {
 
     public static void main(String[] args) {
         
-        Restaurant r1 = new Restaurant("California Pizza Kitchen", 600, 20.00),
-                   r2 = new Restaurant("The Stand", 760, 15.00);
+        Restaurant r1 = new Restaurant("California Pizza Kitchen", 200000, 30.00),
+                   r2 = new Restaurant("The Stand", 250000, 35.00);
         
-        System.out.print(r1.toString() + "\n");
-        System.out.print(r2.toString() + "\n");
+        System.out.println( r1.toString() );
+        System.out.println( r2.toString() );
         
         //Setting r2.peopleServed to r1.peopleServed
-        System.out.print( "Changing r2's number of people served  to " 
-            + r1.getPeopleServed() + "\n" );
+        System.out.println( "Changing r2's number of people served to " 
+            + r1.getPeopleServed() );
         r2.setPeopleServed( r1.getPeopleServed() );
         
         //Setting r2.averagePrice to r1.averagePrice
-        System.out.print( "Changing r2's average meal price to " 
-            + r1.getAveragePrice() + "\n"
-            + "\n" );
+        System.out.println( "Changing r2's average meal price to " 
+            + r1.getAveragePrice() + "\n" );
         r2.setAveragePrice( r1.getAveragePrice() );
         
         //Comparing r1 and r2
-        System.out.print( "Are restaurant 1(r1) and restaurant 2(r2) the same? "
-            + r1.equals( r2 ) + "\n" );
+        System.out.println( "Are restaurant 1(r1) and restaurant 2(r2) the same? "
+            + r1.equals( r2 ) );
         
         //Setting r2.name to r1.name
-        System.out.print( "Changing r2's store name to "
-            + r1.getName() + "\n"
-            + "\n" );
+        System.out.println( "Changing r2's store name to "
+            + r1.getName() + "\n" );
         r2.setName( r1.getName() );
         
         //Comparing r1 and r2
-        System.out.print( "Are restaurant 1(r1) and restaurant 2(r2) the same? "
-            + r1.equals( r2 ) + "\n" );        
+        System.out.println( "Are restaurant 1(r1) and restaurant 2(r2) the same? "
+            + r1.equals( r2 ) );        
         
-        System.out.print(" ");
+        //Decided not to use DecimalFormat for this, price is already double
+        System.out.println("Average price per person: $" + r1.getAveragePrice() );
         
         System.exit( 0 );
         
