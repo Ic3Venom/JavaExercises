8fb168140c7a7d8d646f110ed08e7687438d9a8a
Ic3Venom <Ic3Venom@gmail.com>
2017-06-30 23:00:32 -0700

    Update graphicsshelltest.java

--- GraphicsShellTest/graphicsshelltest.java
+++ GraphicsShellTest/graphicsshelltest.java
@@ -3,25 +3,31 @@ package graphicsshelltest;
 
 import java.awt.Graphics;
 import java.awt.Color;
+import java.awt.image.BufferedImage;
 import javax.swing.JFrame;
 
 public class GraphicsShellTest extends JFrame{
+    private final static GraphicsShellTest app = new GraphicsShellTest( );
+    public  final static int HEIGHT = 1000;
+    public  final static int WIDTH  = 1000;
     
-    public void paint( Graphics g ) {
-        super.paint( g );
+    public void paint( Graphics app ) {
+        super.paint( app );
 
-        g.setColor( Color.RED );
-        g.fillRect( 100, 70, 50, 50 );
+        app.setColor( Color.RED );
+        app.fillRect( 100, 70, 50, 50 );
 
-        g.setColor( Color.BLUE );
-        g.fillRect( 200, 150, 75, 75 ); 
+        app.setColor( Color.BLUE );
+        app.fillRect( 200, 150, 75, 75 ); 
     }
     
     public static void main(String[] args) {
-        GraphicsShellTest app = new GraphicsShellTest( );
         
-        app.setSize( 400, 300 );
+        app.setSize( HEIGHT, WIDTH );
+        
+        app.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         app.setVisible( true );
+    
     }
     
 }
