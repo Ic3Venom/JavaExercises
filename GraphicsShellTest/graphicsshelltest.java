5929ba5d53e112010085c17b0ae94a17f9328acf
Ic3Venom <Ic3Venom@gmail.com>
2017-07-01 22:44:17 -0700

    Testing
    Tinkered with moving around variables, created a grayscale spectrum.

--- GraphicsShellTest/graphicsshelltest.java
+++ GraphicsShellTest/graphicsshelltest.java
@@ -3,27 +3,38 @@ package graphicsshelltest;
 
 import java.awt.Graphics;
 import java.awt.Color;
-import java.awt.image.BufferedImage;
 import javax.swing.JFrame;
 
 public class GraphicsShellTest extends JFrame{
-    private final static GraphicsShellTest app = new GraphicsShellTest( );
-    public  final static int HEIGHT = 1000;
-    public  final static int WIDTH  = 1000;
+     
     
     public void paint( Graphics app ) {
         super.paint( app );
-
+        
+        for(int i = 0; i < 255; i++){
+            Color c = new Color(i, i, i);
+            
+            app.setColor( c );
+            
+            app.drawLine(100, i, 120, i);
+            
+        }
+        
         app.setColor( Color.RED );
         app.fillRect( 100, 70, 50, 50 );
 
         app.setColor( Color.BLUE );
         app.fillRect( 200, 150, 75, 75 ); 
+        
     }
     
     public static void main(String[] args) {
+        final GraphicsShellTest app = new GraphicsShellTest( );
+        final int HEIGHT = 1000;
+        final int WIDTH  = 1000;
         
         app.setSize( HEIGHT, WIDTH );
+        
         
         app.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         app.setVisible( true );
