64c5d776828e2c1d2848883cc2603e0623958d57
Ic3Venom <Ic3Venom@gmail.com>
2017-07-06 23:13:34 -0700

    Add files via upload

---
+++ addlinenumbers/AddLineNumbers.java
@@ -0,0 +1,55 @@
+
+package addlinenumbers;
+
+import java.util.Scanner;
+import java.io.*;
+
+public class AddLineNumbers {
+
+    public static void main( String[] args ) {
+        
+        FileInputStream inputFile;
+        FileOutputStream outputFile;
+        int lineNumber;
+        
+        try 
+        {
+            
+            inputFile = new FileInputStream( "dataInput.txt" );
+            outputFile = new FileOutputStream( "dataOutput.txt" );
+            
+            Scanner scan = new Scanner( inputFile );
+            PrintWriter pw = new PrintWriter( outputFile );
+                        
+            while ( scan.hasNext() )
+            {
+                
+                String line = scan.nextLine();
+                
+                pw.append( line );
+                
+                
+            }
+            
+            outputFile.close();
+            inputFile.close();
+            
+            
+            
+        } 
+        catch (FileNotFoundException e)
+        {
+            System.err.println("Error: " + e.getMessage() + " " + e.toString());
+            e.printStackTrace();
+        }
+        catch ( IOException e )
+        {
+            System.err.println("Error: " + e.getMessage() + " " + e.toString());
+            e.printStackTrace();
+        }
+        
+                
+        
+    }
+    
+}
