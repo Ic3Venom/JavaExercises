9471b399409d9a4c3a54e854c1a492786b8d2738
Ic3Venom <Ic3Venom@gmail.com>
2017-07-10 13:41:48 -0700

    Update AddLineNumbers.java

--- addlinenumbers/AddLineNumbers.java
+++ addlinenumbers/AddLineNumbers.java
@@ -1,3 +1,6 @@
+/*Fifth Java Exersice 
+  Created by Julian Meyn
+*/
 
 package addlinenumbers;
 
@@ -8,15 +11,22 @@ public class AddLineNumbers {
 
     public static void main( String[] args ) {
         
-        FileInputStream inputFile;
-        FileOutputStream outputFile;
-        int lineNumber;
+        FileReader inputFile;
+        FileWriter outputFile;
+        int lineNumber = 1; //Using 1 instead of 0 for non-programming readers
         
         try 
         {
             
-            inputFile = new FileInputStream( "dataInput.txt" );
-            outputFile = new FileOutputStream( "dataOutput.txt" );
+            /*Oracle's Java API on InputFileReader and OutputFileReader say that: 
+                * "FileInputStream is meant for reading streams of raw bytes such as image data. 
+                    For reading streams of characters, consider using FileReader."
+                * "FileOutputStream is meant for writing streams of raw bytes such as image data. 
+                    For writing streams of characters, consider using FileWriter."
+                Thus, for this exersice FileReader and FileWriter are used
+            */
+            inputFile = new FileReader( "dataInput.txt" );
+            outputFile = new FileWriter( "dataOutput.txt" );
             
             Scanner scan = new Scanner( inputFile );
             PrintWriter pw = new PrintWriter( outputFile );
@@ -24,32 +34,36 @@ public class AddLineNumbers {
             while ( scan.hasNext() )
             {
                 
-                String line = scan.nextLine();
+                String fileLine = scan.nextLine();
                 
+                //println used for the automatic newline added
+                //no spacing between lineNumber and line in output file
+                pw.println(lineNumber + fileLine);
                 
+                lineNumber++;
             }
             
+            //Files must close correctly before text is printed, else catch is run
+            scan.close();
+            System.out.println("Input file has been read and closed.");
+            pw.close();
+            System.out.println("Output file has been written and closed.");
             
         } 
         catch (FileNotFoundException e)
         {
             System.err.println("Error: " + e.getMessage() + " " + e.toString());
             e.printStackTrace();
+            System.exit( 1 );
         }
         catch ( IOException e )
         {
             System.err.println("Error: " + e.getMessage() + " " + e.toString());
             e.printStackTrace();
-        }
-        finally
-        {
-            //TODO: add system.out messages about closing files
-            inputFile.close();
-            outputFile.close();
-            
+            System.exit( 1 );
         }
         
-                
+        System.exit( 0 );
         
     }
     
