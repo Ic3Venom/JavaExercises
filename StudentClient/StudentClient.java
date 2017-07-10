c17060ebdf096abfa5cd52668c895a9509c0e54a
Ic3Venom <Ic3Venom@gmail.com>
2017-07-09 23:43:19 -0700

    Update StudentClient.java

--- StudentClient/StudentClient.java
+++ StudentClient/StudentClient.java
@@ -10,35 +10,33 @@ public class StudentClient {
                 s2 = new Student("Charisse", "314159265", 3.76);
         
         //Printing out the information of s1 using the getInfo methods
-        System.out.print("Name: " + s1.getName() + "\n"
+        System.out.println("Name: " + s1.getName() + "\n"
                 + "Social security number: " + s1.getSSN() + "\n"
-                + "Grade point average: " + s1.getGPA()  + "\n\n");
+                + "Grade point average: " + s1.getGPA()  + "\n" );
         
         //Printing out the information of s2 using the toString() method
         System.out.print( s2.toString() );
         
         //Comparing s1 and s2 to see if they are equal
-        System.out.print( "Student 1 and student 2 are the same person: "
-                + s1.equals(s2) 
-                + "\n\n");
+        System.out.println( "Student 1 and student 2 are the same person: "
+                + s1.equals(s2) + "\n );
         
         //Changing the information of s2 to the same information as s1
-        System.out.print("Changing student 2's name from \"" + s2.getName()
+        System.out.println( "Changing student 2's name from \"" + s2.getName()
                 + "\" to Julian\"\n"
                 + "Changing student 2's GPA from \"" + s2.getGPA()
                 + "\" to \"4.00\"\n"
                 + "Changing student 2's SSN from \"" + s2.getSSN()
-                + "\" to \"123456789\"\n\n");
+                + "\" to \"123456789\"\n" );
         s2.setName("Julian");
         s2.setSSN("123456789");
         s2.setGPA(4.00);
         
         //Comparing s1 and s2 again to see if they are equal
-        System.out.print( "Student 1 and student 2 are the same person: "
-                + s1.equals(s2) 
-                + "\n");
+        System.out.println( "Student 1 and student 2 are the same person: "
+                + s1.equals(s2) ");
         
-        System.out.print("\nExiting program...\n");
+        System.out.println( "\nExiting program..." );
         System.exit( 0 );
         
     }
