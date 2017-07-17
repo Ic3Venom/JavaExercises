390f86a382584d95c0889faa5f8fcdc70621a7b6
Ic3Venom <Ic3Venom@gmail.com>
2017-07-16 23:02:13 -0700

    Add files via upload

---
+++ midtermtesting.java
@@ -0,0 +1,52 @@
+/* Random testings before the midterm
+   *Not an actual exercise*
+*/
+
+package midtermtesting;
+
+public class midtermtesting {
+
+    public static void main(String[] args) {
+        
+        boolean a = true;
+        boolean b = false;
+        System.out.println(!(a&&b));
+                
+        String c = "awesome";
+        String d = "bwesome";
+        System.out.println(c.compareTo(d));
+        
+        int e = 0;
+        int f = 1;
+        System.out.print("test3: ");
+        if(e>0)
+        {
+            System.out.println(e);
+        }
+        else
+        {
+            System.out.println(f);
+        }
+        
+        if true
+                if true
+                        if true
+                                if true
+                                        System.out.print("compiler no likey, but it should work?");
+        
+        int g = 1;
+        System.out.println();
+        while true
+        {
+            System.out.println("test4");
+            if (g > 10)
+            {
+                break;
+            }
+            
+            g++;
+        }   
+        
+    }
+    
+}
